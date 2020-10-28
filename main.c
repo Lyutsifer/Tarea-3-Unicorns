4c4727d1cda835ad9c10276a6046c22faa9bc112
Lyutsifer <71039758+Lyutsifer@users.noreply.github.com>
2020-10-28 16:25:44 -0300

    Update main.c
    Short menu

--- main.c
+++ main.c
@@ -4,9 +4,32 @@
 #include "funciones.h"
 int main()
 {
+    int option;
     HashMap * Ciudades = createMap(200);
+    while (1){
+        printf("\nWelcome to the Unicorn's City (n _ n) What would you like to do?  \n\n");
+        printf("Option 1 <3 : Import cities from a file \n");
+        printf("Option 2 <3 : Show the nearest city \n");
+        printf("Option 3 <3 : Create a route \n");
+        printf("Option 4 <3 : Search the shortest route between cities  \n");
+        printf("Option 5 <3 : Best circuit \n");
+        scanf ("%d", &option);
 
-     ImportarCiudades(Ciudades);
+        switch (option){
+            case 1: ImportarCiudades(Ciudades);
+            break;
+            /* case 2:
+            break;
+            case 4:
+            break
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break; */
+        }
 
+    }
     return 0;
 }
